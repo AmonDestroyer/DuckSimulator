b4e07c0f6f0724502a7e318ba6d643f679771fb0
James Taylor <solitarebecauseneon@gmail.com>
2022-04-24 15:15:16 -0700

    Jump limiter implemented AND lots of ASSETS!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,23 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 0.2f;
     public float height = 10.0f;
+    public int jumpNum = 2;
     public bool debug = false;
-    public PlayerInput palyerInput;
+    public PlayerInput playerInput;
 
     private Rigidbody player;
     private Vector3 movement;
     private float movementX;
     private float movementY;
+    private bool onGround;
+    private int jumpCurrent;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody>();
+        onGround = true;
+        jumpCurrent = 0;
     }
 
     void OnMove(InputValue movementValue)
@@ -34,13 +39,24 @@ public class PlayerController : MonoBehaviour
 
     void OnJump()
     {
-        Vector3 jump = new Vector3(0.0f, height, 0.0f);
-        player.AddForce(jump, ForceMode.Impulse);
+        if(onGround == true || jumpCurrent < jumpNum) {
+            Vector3 jump = new Vector3(0.0f, height, 0.0f);
+            player.AddForce(jump, ForceMode.Impulse);
+            onGround = false;
+            jumpCurrent += 1;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         player.MovePosition(player.position + movement * speed);
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Ground")) {
+            onGround = true;
+            jumpCurrent = 0;
+        }
     }
 }
