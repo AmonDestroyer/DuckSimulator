db06b0633b241b36c13aa248f4c311c6fb858b50
Adam Case <adamrichcase@gmail.com>
2022-04-25 00:11:38 -0700

    Implement yaw look and improved movement based on look

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0.2f;
-    public float height = 10.0f;
+    public float walkSpeed = 0.2f;
+    public float lookSpeed = 100.0f;
+    public float jumpHeight = 10.0f;
     public float gravityStrength = -1.0f;
     public int jumpNum = 2;
     public bool debug = false;
@@ -15,8 +16,9 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody player;
     private Vector3 movement;
-    private float movementX;
-    private float movementY;
+    private Quaternion look = Quaternion.identity;
+    private float movementX, movementY;
+    private float lookX, lookY;
     private bool onGround;
     private int jumpCurrent;
     private Vector3 gravStr;
@@ -28,6 +30,7 @@ public class PlayerController : MonoBehaviour
         player = GetComponent<Rigidbody>();
         onGround = true;
         jumpCurrent = 0;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void OnMove(InputValue movementValue)
@@ -37,14 +40,25 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
         if (debug)
             Debug.Log($"Input: ({movementX},{movementY})");
-        movement = new Vector3(movementX, 0.0f, movementY);
+    }
 
+    void OnLook(InputValue lookValue)
+    {
+        Vector2 lookVector = lookValue.Get<Vector2>();
+        lookX = lookVector.x;
+        lookY = lookVector.y; //Right now up and down look is not presently working
+        if (debug)
+            Debug.Log($"Look: ({lookX},{lookY})");
+        Vector3 rotation = Quaternion.AngleAxis(lookX, transform.up) * transform.forward;
+        Vector3 lookDirection = Vector3.RotateTowards(transform.forward, rotation, lookSpeed * Time.deltaTime, 0f);
+        lookDirection.Normalize();
+        look = Quaternion.LookRotation(lookDirection);
     }
 
     void OnJump()
     {
         if(onGround == true || jumpCurrent < jumpNum) {
-            Vector3 jump = new Vector3(0.0f, height, 0.0f);
+            Vector3 jump = new Vector3(0.0f, jumpHeight, 0.0f);
             player.AddForce(jump, ForceMode.Impulse);
             onGround = false;
             jumpCurrent += 1;
@@ -55,7 +69,9 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Physics.gravity = gravStr;
-        player.MovePosition(player.position + movement * speed);
+        movement = transform.forward * movementY + transform.right * movementX;
+        player.MovePosition(player.position + movement * walkSpeed);
+        player.MoveRotation(look);
     }
 
     void OnCollisionEnter(Collision other) {
