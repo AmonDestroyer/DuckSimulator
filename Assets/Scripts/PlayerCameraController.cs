c3374d82dabce69e220e09e9e96fa18d067ff311
Adam Case <adamrichcase@gmail.com>
2022-04-26 00:26:56 -0700

    Implement third person look

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/*
+Description: Adjuststs camera based on follow target part of the player
+References:
+- Creating a Third Person Camera using Cinemachine in Unity! (Tutorial)
+  - Creator: Unity
+  - Link: https://www.youtube.com/watch?v=537B1kJp9YQ
+*/
+
+public class PlayerCameraController : MonoBehaviour
+{
+
+    public PlayerInput playerInput;
+    public GameObject followTransform;
+    public Camera camera;
+    public float pitchSpeed = 1f;
+    public float yawSpeed = 1f;
+
+    private Vector2 _look;
+
+    void OnLook(InputValue lookValue)
+    {
+        _look = lookValue.Get<Vector2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //Rotate the Follow Target transform based on the input
+      followTransform.transform.rotation *= Quaternion.AngleAxis(_look.x * yawSpeed, Vector3.up);
+      //yaw
+      followTransform.transform.rotation *= Quaternion.AngleAxis(-_look.y * pitchSpeed, Vector3.right);
+      //pitch
+
+      //Set basic look angles
+      var angles = followTransform.transform.localEulerAngles;
+      angles.z = 0;
+
+      var angle = followTransform.transform.localEulerAngles.x;
+      //Clamp the Up/Down rotation
+      if (angle > 180 && angle < 350) //Up (cCmera below player)
+      {
+          angles.x = 350;
+      }
+      else if(angle < 180 && angle > 70) //Down (Camera above player)
+      {
+          angles.x = 70;
+      }
+
+      followTransform.transform.localEulerAngles = angles;
+
+      //Set the player rotation based on the look transform
+      transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
+      //reset the y rotation of the look transform
+      followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,6 @@ using UnityEngine.InputSystem.Interactions;
 
 public class PlayerController : MonoBehaviour
 {
-    public float lookSpeed = 100.0f;
     public float jumpHeight = 10.0f;
     public float walkSpeed = 0.2f;
     public float sprintSpeed = 0.4f;
@@ -16,12 +15,10 @@ public class PlayerController : MonoBehaviour
     public float glideMulti = 0.1f;
     public bool debug = false;
     public PlayerInput playerInput;
-    
 
     private Rigidbody player;
     // movement variables
     private Vector3 movement;
-    private Quaternion look = Quaternion.identity;
     private float movementX, movementY;
     private float lookX, lookY;
     private bool isSprint;
@@ -35,7 +32,7 @@ public class PlayerController : MonoBehaviour
     private InputAction jumpAction;
     private InputAction sprintAction;
     private InputAction crouchAction;
-    
+
 
 
     // Start is called before the first frame update
@@ -44,8 +41,7 @@ public class PlayerController : MonoBehaviour
         jumpAction = playerInput.currentActionMap["Jump"];
         sprintAction = playerInput.currentActionMap["Sprint"];
         crouchAction = playerInput.currentActionMap["Crouch"];
-        player = GetComponent<Rigidbody>();
-        
+        player = GetComponent<Rigidbody>();;
     }
 
     void Start()
@@ -64,19 +60,6 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
         if (debug)
             Debug.Log($"Input: ({movementX},{movementY})");
-    }
-
-    void OnLook(InputValue lookValue)
-    {
-        Vector2 lookVector = lookValue.Get<Vector2>();
-        lookX = lookVector.x;
-        lookY = lookVector.y; //Right now up and down look is not presently working
-        if (debug)
-            Debug.Log($"Look: ({lookX},{lookY})");
-        Vector3 rotation = Quaternion.AngleAxis(lookX, transform.up) * transform.forward;
-        Vector3 lookDirection = Vector3.RotateTowards(transform.forward, rotation, lookSpeed * Time.deltaTime, 0f);
-        lookDirection.Normalize();
-        look = Quaternion.LookRotation(lookDirection);
     }
 
     private void SetDefaultMovement()
@@ -112,7 +95,7 @@ public class PlayerController : MonoBehaviour
         isGlide = true; // for update usage
         jumpCurrent = jumpNum; // no more jumping while gliding
         // stops vertical acceleration
-        Vector3 vert_cancel = new Vector3(movementX, 0.0f, movementY); 
+        Vector3 vert_cancel = new Vector3(movementX, 0.0f, movementY);
         player.velocity = vert_cancel;
         // used for making gravity calculation over time
         gravOpposite = 0.0f;
@@ -173,6 +156,7 @@ public class PlayerController : MonoBehaviour
             isCrouch = false;
         };
     }
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -196,7 +180,6 @@ public class PlayerController : MonoBehaviour
         {
             player.MovePosition(player.position + movement * walkSpeed);
         }
-        player.MoveRotation(look);
     }
 
     void OnCollisionEnter(Collision other) {
