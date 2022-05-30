4f3782c3d8cd87c8349f78d9e3886380fc42058f
Adam Case <adamrichcase@gmail.com>
2022-05-30 01:42:41 -0700

    Bug Correction: Working Exit Menu Exit menu will not allow proper exit to main menu

--- Assets/Scripts/Menu_ExitController.cs
+++ Assets/Scripts/Menu_ExitController.cs
@@ -13,6 +13,7 @@ public class Menu_ExitController : MonoBehaviour
     public Button m_BtnMainMenu;
 
     private GameObject player;
+    private AnySceneManager anySceneManager;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,7 @@ public class Menu_ExitController : MonoBehaviour
       m_BtnResume.onClick.AddListener(Resume);
       m_BtnMainMenu.onClick.AddListener(MainMenu);
       player = GameObject.Find("Player");
+      anySceneManager = GameObject.FindGameObjectWithTag("sceneManager").GetComponentInChildren(typeof(AnySceneManager)) as AnySceneManager;
     }
 
     // Update is called once per frame
@@ -36,5 +38,7 @@ public class Menu_ExitController : MonoBehaviour
     void MainMenu()
     {
       SceneManager.LoadScene("MainScene");
+      int currentScene = anySceneManager.m_NewScene;
+      anySceneManager.TransitionScene(1, currentScene);
     }
 }
