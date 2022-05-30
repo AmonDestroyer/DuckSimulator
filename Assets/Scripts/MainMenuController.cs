a1a4174e6902872dd657d2c8cb15793c21e97a33
Adam Case <adamrichcase@gmail.com>
2022-05-30 01:36:59 -0700

    Options Button Linked While disabled the options button was linked to the Main Menu Controller and an arbirtrary class was created.

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -8,23 +8,17 @@ public class MainMenuController : MonoBehaviour
 {
     public Button btnTutorial;
     public Button btnNewGame;
-    //public int fadeTime = 3;
-    //public CanvasGroup black;
-    //public TextMeshProUGUI LoadPercentage;
+    public Button btnOptions;
+
+    //Private Variables Start
     private AnySceneManager m_AnySceneManager;
-    /*
-    private bool fade = false;
-    private float m_Timer;
-    private string loadScene = "";
-    private AsyncOperation asyncLoad;
-    */
+
     // Start is called before the first frame update
     void Start()
     {
-      //black = GetComponent<CanvasGroup>();
-      //MainManager = FindObjectOfType<NeverUnloadSceneManager>();
       btnTutorial.onClick.AddListener(LaunchTutorial);
       btnNewGame.onClick.AddListener(NewGame);
+      btnOptions.onClick.AddListener(Options);
       Cursor.lockState = CursorLockMode.None;//locked by the playerController script
 
       m_AnySceneManager = GameObject.FindGameObjectWithTag("sceneManager").GetComponentInChildren(typeof(AnySceneManager)) as AnySceneManager;
@@ -32,12 +26,13 @@ public class MainMenuController : MonoBehaviour
       if(m_AnySceneManager == null) { // try again, but with inactive objects too
         m_AnySceneManager = GameObject.FindGameObjectWithTag("sceneManager").GetComponentInChildren(typeof(AnySceneManager), true) as AnySceneManager;
       }
+
     }
 
     // Update is called once per frame
     void Update()
     {
- 
+
     }
 
     /*
@@ -45,23 +40,17 @@ public class MainMenuController : MonoBehaviour
     */
     void LaunchTutorial()
     {
-      //MainManager.StartMainScene();
-      //loadScene = "TutorialScene";
       m_AnySceneManager.TransitionScene(2, 1);
     }
 
     void NewGame()
     {
-      //MainManager.StartMainScene();
-      //loadScene = "HomeBaseScene";
       m_AnySceneManager.TransitionScene(3, 1);
     }
 
-    /*void LoadScene()
+    void Options()
     {
-      Debug.Log($"Loading: {loadScene}");
-      asyncLoad = SceneManager.LoadSceneAsync(loadScene);
-      m_Timer = 0f;
-      fade = true;
-    }*/
+      //Arbirary class used for when the options menu is selected
+    }
+
 }
