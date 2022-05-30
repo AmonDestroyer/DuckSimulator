07c08a6767379ae7a626e0e8aabbbc8829919b4b
Adam Case <adamrichcase@gmail.com>
2022-05-30 01:51:07 -0700

    Bug Correction: Ability Activation Activates avilities appropriately prior to starting a new game

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -9,15 +9,23 @@ public class Portal : MonoBehaviour
     public int newScene;
     public int currentScene;
     //public NeverUnloadSceneManager MainManager;
+    private PlayerController m_playerController;
     public AnySceneManager anySceneManager;
+
+
 
     void Start() {
       anySceneManager = GameObject.FindGameObjectWithTag("sceneManager").GetComponentInChildren(typeof(AnySceneManager)) as AnySceneManager;
+      m_playerController = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<PlayerController>();
+      m_playerController.jumpNum = 2;
+      m_playerController.enableFire = true;
+      m_playerController.sprintSpeed = 0.4f;
+
       m_playerController.stdTime();
     }
 
     void OnUpdate(){
-      
+
     }
 
     void OnTriggerEnter(Collider other){
